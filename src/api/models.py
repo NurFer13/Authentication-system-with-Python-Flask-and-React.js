7bc9aa68d741286f9b818ecd48355b217fb7bd6d
NurFer13 <nurfer1307@gmail.com>
2023-04-28 12:58:21 +0000

    casi

--- src/api/models.py
+++ src/api/models.py
@@ -6,14 +6,24 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
+
+    def __init__(self,email,password,is_active):
+       
+            self.email=email
+            self.password=password
+            self.is_active=is_active
+        
+        
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
+        
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,48 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/login', methods = ['POST'])
+def login():
+    body =request.get_json()
+    login=User.query.filter_by(email=body["email"]).first()
+    if login:
+        token=create_access_token(identity=login.serialize())
+        return jsonify(token)
+    else:
+        return jsonify({"msg":"user not found","status":404,"tu":"pringao"}),404
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route('/signup', methods = ['POST'])
+def signup():
+    body = request.get_json()
+    new_user = User(body["email"], body["password"], True)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()),201
 
-    return jsonify(response_body), 200
+
+@api.route('/user/<int:id>', methods = ['GET'])
+def get_user_by_id(id):
+    user=User.query.get(id)
+
+    return jsonify(user.serialize()),200
+
+@api.route('/user', methods = ['GET'])
+def get_users():            
+    all_users=User.query.all()  
+    all_users=list(map(lambda user:user.serialize(),all_users)) 
+
+    return jsonify(all_users),200
+
+@api.route('/user/delete', methods = ['POST'])
+def delete_user(id):
+    user=User.query.get(id).first()
+    db.session.remove(user)
+    db.session.commit()
+    return jsonify(user.serialize()),201
